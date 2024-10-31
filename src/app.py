4fab5deed8e96a0bf6b839fa872e9fd42c81318d
fgamester <falvarez.munoz@hotmail.es>
2024-10-31 13:03:11 -0300

    models completed

--- src/app.py
+++ src/app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db
+from dotenv import load_dotenv
+from sqlalchemy import inspect
 
-app = Flask(__name__)
+load_dotenv()
+
+PATH = os.path.abspath('instance')
+
+app = Flask(__name__, instance_path=PATH)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+
+db.init_app(app)
+Migrate(app, db)
+CORS(app)
+
+@app.route('/tables', methods=['GET'])
+def list_tables():
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+    return jsonify(tables)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/models.py
@@ -0,0 +1,92 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+favorite_planets = db.Table(
+    'favorites_planets',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('planet_id', db.Integer, db.ForeignKey('planets.id'), primary_key=True)
+)
+
+favorite_characters = db.Table(
+    'favorites_characters',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('character_id', db.Integer, db.ForeignKey('characters.id'), primary_key=True)
+)
+	
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    favorites_planets = db.relationship('Planet', secondary='favorites_planets')
+    favorites_characters = db.relationship('Character', secondary='favorites_characters')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'favorites_planets': [{'planet_id' : planet['planet_id']} for planet in self.favorites_planets],
+            'favorites_characters': [{'character_id' : character['character_id']} for character in self.favorites_characters]
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
